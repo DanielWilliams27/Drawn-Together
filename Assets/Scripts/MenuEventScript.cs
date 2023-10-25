46fa8bc35dcba45a79257b7e76eb198c109d4986
DanielWilliams27 <danielwilliams2791997@gmail.com>
2023-10-25 16:46:28 +0100

    Replaced buttons with dropdown

--- Assets/Scripts/MenuEventScript.cs
+++ Assets/Scripts/MenuEventScript.cs
@@ -10,7 +10,6 @@ public class MenuEventScript : MonoBehaviour
     public GameObject main;
     public GameObject options;
     public GameObject levels;
-    public Text upText, downText, leftText, rightText;
 
     public void LoadScene(string scene)
     {
@@ -27,9 +26,5 @@ public class MenuEventScript : MonoBehaviour
     {
         deactivateMenu.SetActive(false);
         options.SetActive(true);
-        upText.text = GlobalScript.up.ToString();
-        downText.text = GlobalScript.down.ToString();
-        leftText.text = GlobalScript.left.ToString();
-        rightText.text = GlobalScript.right.ToString();
     }
 }
